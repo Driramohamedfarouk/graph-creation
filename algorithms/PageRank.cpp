e36e6feb1c03de633542a6c46a6835a3bdfa4ac5
Driramohamedfarouk <mohamedfaroukdrira@insat.u-carthage.tn>
2023-08-04 22:55:02 +0100

    using C++ SIMD intrinsics for PageRank

--- algorithms/PageRank.cpp
+++ algorithms/PageRank.cpp
@@ -9,6 +9,13 @@
 #include <fstream>
 #include <numeric>
 #include "../graph-creation/getDstFile.h"
+#include <immintrin.h>
+#include <cassert>
+
+
+#define d 0.85f
+//#define cilk_for for
+
 
 void parallelPageRank(const std::string& path, const int n, const int nb_iteration){
 
@@ -17,52 +24,78 @@ void parallelPageRank(const std::string& path, const int n, const int nb_iterati
     int *dst = x.first ,nb_edges = x.second ;
 
     ExtendedPairEdgeCentric g = BranchlessCreateGraphFromFilePageRank(path,n,nb_edges);
-
     std::cout << "loaded the graph in memory " << '\n' ;
+
+
+    // TODO : construct an float array of inverses of degrees
+
 
     auto start = std::chrono::high_resolution_clock::now();
 
     // allocate two arrays of size n
     // one contains PR for previous iteration
     // and one contains PR for current iteration
-    auto* previousPR = static_cast<double *>(malloc(n * sizeof(double)));
-    auto* PR = static_cast<double *>(malloc(n * sizeof(double)));
-    double* temp ;
+    //std::cout << n/8 +1 << '\n' ;
+    auto* previousPR = static_cast<float *>(std::aligned_alloc(alignof(__m256),(n/8+1) * sizeof(__m256 )));
+    auto* PR = static_cast<float *>(std::aligned_alloc(alignof(__m256),(n/8+1) * sizeof(__m256 )));
+    float * temp ;
 
-    // damping factor of pagerank
-    const float d =0.85;
 
-    for (int i = 0; i < n; ++i) {
-        previousPR[i] = 1.0/n ;
+    const float y = 1.0f/(float )n ;
+    const float z = y*(1-d) ;
+
+    __m256 avx_y = _mm256_set1_ps(y);
+
+    __m256 avx_z = _mm256_set1_ps(z);
+
+
+//#pragma omp parallel for
+    int j = 0 ;
+    for(; j < n - 8  ; j+=8) {
+        _mm256_store_ps(previousPR+j, avx_y);
     }
+    for (; j < n; ++j) {
+        previousPR[j] = y ;
+    }
+    __m256 avx_zeroes = _mm256_set1_ps(0.0f) ;
+
+    __m256 avx_d = _mm256_set1_ps(d);
 
     for (int i = 0; i < nb_iteration; ++i) {
-
-        for (int  j = 0; j < n; ++j) {
-            PR[j] = 0.0 ;
+//#pragma omp parallel for
+        int m = 0 ;
+        for(; m < n-8; m+=8) {
+            _mm256_store_ps(PR + m, avx_zeroes);
+        }
+        for (; m < n; ++m) {
+            PR[m] = 0.0f ;
+        }
+        // in a fully parallel manner update previous to previous[m] /= out_degree[m]
+        //#pragma omp parallel for
+        for (int s = 0; s < n; ++s) {
+            previousPR[s] = previousPR[s] /(float ) g.out_degree[s]  ;
         }
 
-        // in a fully parallel manner update previous to previous[i] /= out_degree[i]
-//        #pragma omp parallel for
-//        for (int j = 0; j < n; ++j) {
-//            previousPR[j] = (previousPR[j]!=0)* (previousPR[j]/g.out_degree[j])  ;
-//        }
-
+        //#pragma grainsize 256
         #pragma omp parallel for schedule(dynamic,256)
-        for (int j = 0; j < g.src_size ; ++j) {
+        for (int l = 0; l < g.src_size ; ++l) {
             // too much random access here
-            for (int k = g.src[j].second; k < g.src[j+1].second ; ++k) {
+            for (int k = g.src[l].second; k < g.src[l + 1].second ; ++k) {
                 #pragma omp atomic
-                PR[dst[k]]+= previousPR[g.src[j].first]/g.out_degree[g.src[j].first] ;
+                PR[dst[k]]+= previousPR[g.src[l].first] ;
             }
         }
-
-        #pragma omp parallel for
-        for (int j = 0; j < n; ++j) {
-            PR[j] = d*PR[j] + (1 -d)/(float)n ;
+        //#pragma omp parallel for
+        int l = 0 ;
+        for(; l < n; l+=8) {
+            __m256 avx_PR = _mm256_loadu_ps(&PR[l]);
+            avx_PR = _mm256_fmadd_ps(avx_d,avx_PR,avx_z);
+            _mm256_storeu_ps(&PR[l],avx_PR) ;
         }
-
-       // swap PR and previousPR
+        for (;l < n ; ++l) {
+            PR[l] = d * PR[l] + z ;
+        }
+        // swap PR and previousPR
         temp = previousPR ;
         previousPR = PR ;
         PR  =  temp ;
